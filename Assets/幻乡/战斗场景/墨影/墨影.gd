6b89b39050956e18ef49e22ede808be890f646f8
Mumu <linjiamu49@gmail.com>
2025-03-06 02:04:00 -0500

    2025/3/6

---
+++ Assets/幻乡/战斗场景/墨影/墨影.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D # 身法哥画的白影子
+
+var scene_startx: float = 0.0
+var scene_endx: float = 1400.0
+var health = 1400
+var direct = Vector2.LEFT
+
+func _ready() -> void:
+	add_to_group("shadows")
+	add_to_group("monster")
+	$StateMachine.change_state(0)
+
+func take_hit(value: int):
+	if health <= 0:
+		$HitEffectPlayer.play("HitFlash")
+		$StateMachine.change_state(5)
+	else:
+		health -= value
+		$HitEffectPlayer.play("HitFlash")
+		
+func _physics_process(delta: float) -> void:
+	#print($AnimatedSprite2D.animation)
+	pass
